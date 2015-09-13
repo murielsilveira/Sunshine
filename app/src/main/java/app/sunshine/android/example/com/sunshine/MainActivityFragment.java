5a4a634ca2b056fb9c48a870fdd9fcdff2fc5011
Muriel Silveira <murielkong@gmail.com>
2015-09-12 22:18:03 -0300

    Create a list on the main fragment with some forecasts.

--- app/src/main/java/app/sunshine/android/example/com/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/sunshine/android/example/com/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,44 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/46",
+                "Mon - Cloudy - 72/63",
+                "Tue - Rain - 72/63",
+                "Weds - Heavy Rain - 72/63",
+                "Thurs - HELP TRAPPED IN WEATHERSTATION - 72/63",
+                "Fri - Sunny - 72/63",
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/46",
+                "Mon - Cloudy - 72/63",
+                "Tue - Rain - 72/63",
+                "Weds - Heavy Rain - 72/63",
+                "Thurs - HELP TRAPPED IN WEATHERSTATION - 72/63",
+                "Fri - Sunny - 72/63",
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/46",
+                "Mon - Cloudy - 72/63",
+                "Tue - Rain - 72/63",
+                "Weds - Heavy Rain - 72/63",
+                "Thurs - HELP TRAPPED IN WEATHERSTATION - 72/63",
+                "Fri - Sunny - 72/63",
+        };
+
+        ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 }
