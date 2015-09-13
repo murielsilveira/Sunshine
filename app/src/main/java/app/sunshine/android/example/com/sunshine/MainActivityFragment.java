bae4136d5fa7c7ee2ebe4d5aa97e66b3d082d643
Muriel Silveira <murielkong@gmail.com>
2015-09-13 18:18:41 -0300

    Add toast on item click

--- app/src/main/java/app/sunshine/android/example/com/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/sunshine/android/example/com/sunshine/MainActivityFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -70,6 +72,12 @@ public class MainActivityFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(view.getContext(), adapterView.getAdapter().getItem(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
