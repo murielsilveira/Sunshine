eb7a091d3bdd31faef430a4601f775397a0f4c7f
Muriel Silveira <murielkong@gmail.com>
2016-02-01 23:46:27 -0200

    Test URI matcher and build weather location

--- app/src/androidTest/java/app/sunshine/android/example/com/sunshine/data/TestWeatherContract.java
+++ app/src/androidTest/java/app/sunshine/android/example/com/sunshine/data/TestWeatherContract.java
@@ -7,15 +7,16 @@ import android.test.AndroidTestCase;
 public class TestWeatherContract extends AndroidTestCase {
     // intentionally includes a slash to make sure Uri is getting quoted correctly
     private static final String TEST_WEATHER_LOCATION = "/North Pole";
+    private static final String TEST_WEATHER_LOCATION_URI = "content://app.sunshine.android.example.com.sunshine/weather/%2FNorth%20Pole";
     private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
 
-//    public void testBuildWeatherLocation() {
-//        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
-//
-//        assertNotNull(locationUri);
-//        assertEquals("Error: Weather location not properly appended to the end of the Uri",
-//                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
-//        assertEquals("Error: Weather location Uri doesn't match our expected result",
-//                locationUri.toString(), "content://app.sunshine.android.example.com.sunshine/weather/%2FNorth%20Pole");
-//    }
+    public void testBuildWeatherLocation() {
+        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
+
+        assertNotNull(locationUri);
+        assertEquals("Error: Weather location not properly appended to the end of the Uri",
+                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
+        assertEquals("Error: Weather location Uri doesn't match our expected result",
+                TEST_WEATHER_LOCATION_URI, locationUri.toString());
+    }
 }
